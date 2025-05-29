39a28feb98858f34b5eda8a4a232e6101d579dc8
Fabián Toledo <metraplox@gmail.com>
2025-05-29 04:08:29 -0400

    Importación del esquema de Usuario en el módulo de estudiantes para corregir dependencias

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -3,10 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
 import { Student, StudentSchema } from './schemas/student.schema';
+import { User, UserSchema } from '../users/schemas/user.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Student.name, schema: StudentSchema }]),
+    MongooseModule.forFeature([
+      { name: Student.name, schema: StudentSchema },
+      { name: User.name, schema: UserSchema }
+    ]),
   ],
   controllers: [StudentsController],
   providers: [StudentsService],
