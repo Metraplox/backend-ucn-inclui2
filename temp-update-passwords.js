947571394fb61016713a87d12e10875a7b139ef8
Fabián Toledo <metraplox@gmail.com>
2025-07-06 22:06:45 -0400

    Limpieza final de archivos temporales

--- temp-update-passwords.js
+++
@@ -1,6 +0,0 @@
-db.users.updateMany({}, { 
-  $set: { 
-    password_hash: '$2b$10$n7bkXSJZEIF4jsot6AFe0.60UnBpAmxiH7BK/AVLZ1WYewSp7kNwa' 
-  } 
-});
-print('✅ Todos los usuarios actualizados con password: password123');
--- temp_update_user.js
+++
@@ -1,8 +0,0 @@
-db.users.updateOne(
-  {email: 'coordinador@test.ucn.cl'}, 
-  {$set: {password_hash: '$2b$10$.fcV2z3DuIgNsOcbi/ZWleiXQIxLug1LdCCcjUt34VPuNGGF27M6q'}}
-);
-print('Hash actualizado correctamente');
-
-const user = db.users.findOne({email: 'coordinador@test.ucn.cl'});
-print('Usuario verificado:', JSON.stringify(user));
